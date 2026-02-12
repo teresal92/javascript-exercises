1aa8e7e664924027932b51fd7c03f4dad6e14997
Teresa Lew <teresalew92@gmail.com>
2026-02-12 11:38:45 -0800

    11_tempConversion solution

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+function round(number, base = 10) {
+  return Math.round(number * base) / base;
+}
+
+const convertToCelsius = function (temp) {
+  return round((temp - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  return round((temp * 9) / 5 + 32);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
