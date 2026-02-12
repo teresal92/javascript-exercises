798ed5b5e71debf2199d0b3478143ed195567ef6
Teresa Lew <teresalew92@gmail.com>
2026-02-12 11:23:43 -0800

    10_leapYear solution

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // divisible by 4 AND not divisible by 100 unless they are divisible by 400
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
