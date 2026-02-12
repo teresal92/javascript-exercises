2aede0971baacfdc1c2ea01015d4d06cdd4c8cb7
Teresa Lew <teresalew92@gmail.com>
2026-02-12 11:15:34 -0800

    09_sumAll solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
 
+  if (a > b) {
+    const temp = a;
+    a = b;
+    b = temp;
+  }
+
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
