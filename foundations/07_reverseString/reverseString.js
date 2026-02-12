4b33e7a0a4a433d4d6e16323b0d85a3a03bd5977
Teresa Lew <teresalew92@gmail.com>
2026-02-12 11:00:10 -0800

    07_reverseString solution

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (s) {
+  const chars = s.split("");
+  const reversedChars = [];
 
+  for (let i = chars.length; i >= 0; i--) {
+    reversedChars.push(chars[i]);
+  }
+
+  return reversedChars.join("");
 };
 
 // Do not edit below this line
